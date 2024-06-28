8597622606998625e4a0259309d51d9ced6be6da
edwardsila <edwardsila000@gmail.com>
2024-06-28 12:16:55 +0000

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,70 +1,57 @@
 import Currency from './3-currency';
 
-/**
- * Class representing pricing with amount and currency.
- */
-
 export default class Pricing {
-  /**
-    * Creates an instance of Pricing.
-    * @param {number} amount - The amount of the pricing.
-    * @param {Currency} currency - The currency object (instance of Currency class).
-    */
-
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
+
   /**
-    * Getter for amount attribute.
-    * @returns {number} The current value of amount.
-    */
+   * @param {Number} amount
+   */
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a Number');
+    }
+    this._amount = amount;
+  }
 
   get amount() {
     return this._amount;
   }
 
   /**
-    * Setter for amount attribute.
-    * @param {number} value - The new value to set for amount.
-    */
-
-  set amount(value) {
-    this._amount = value;
+   * @param {Currency} currency
+   */
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('currency must be an instance of Currency');
+    }
   }
 
-  /**
-    * Getter for currency attribute.
-    * @returns {Currency} The current currency object.
-    */
   get currency() {
     return this._currency;
   }
 
-  /**
-    * Setter for currency attribute.
-    * @param {Currency} value - The new currency object to set.
-    */
-
-  set currency(value) {
-    this._currency = value;
-  }
-  /**
-    * Method to display full price with currency details.
-    * @returns {string} Formatted string displaying amount and currency details.
-    */
-
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
-  /**
-    * Static method to convert price using a conversion rate.
-    * @param {number} amount - The amount to convert.
-    * @param {number} conversionRate - The conversion rate to apply.
-    * @returns {number} The converted amount.
-    */
 
+  /**
+   * @param {Number} amount
+   * @param {Number} conversionRate
+   */
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+
     return amount * conversionRate;
   }
 }
