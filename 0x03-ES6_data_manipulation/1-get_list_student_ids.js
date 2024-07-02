932f5e24d5ad2795c64b0ffe32f0e7668533e6ae
edwardsila <edwardsila000@gmail.com>
2024-07-02 19:03:41 +0000

    updated first task

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
-
-  return students.map((student) => student.id);
+  return [];
 }
